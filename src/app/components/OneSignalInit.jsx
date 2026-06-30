0415ad41c366cf16f72f5ebf48f4eaf77ec49d3c
Muhammad Yasir <yasirabbasi0351@gmail.com>
2026-06-30 11:42:35 +0500

    Added

--- src/app/components/OneSignalInit.jsx
+++ src/app/components/OneSignalInit.jsx
@@ -5,21 +5,26 @@ import OneSignal from 'react-onesignal';
 export default function OneSignalInit() {
   useEffect(() => {
     const runOneSignal = async () => {
-      await OneSignal.init({
-        appId: "1cda3b15-73f8-4e66-837d-ab019edf120c",
-        safari_web_id: "web.onesignal.auto.449e8e5d-df22-4f65-ad52-0d8b06e9cdb9", 
-        allowLocalhostAsSecureOrigin: true, 
-        notifyButton: {
-          enable: true,
-        },
-      });
-      // Prompt the user to subscribe
-      OneSignal.Slidedown.promptPush();
+      try {
+        await OneSignal.init({
+          appId: "1cda3b15-73f8-4e66-837d-ab019edf120c",
+          safari_web_id: "web.onesignal.auto.449e8e5d-df22-4f65-ad52-0d8b06e9cdb9", 
+          allowLocalhostAsSecureOrigin: true, 
+          notifyButton: {
+            enable: true,
+          },
+        });
+
+        // Use the native prompt if available
+        await OneSignal.Slidedown.promptPush();
+        
+      } catch (error) {
+        console.error("OneSignal Init Error:", error);
+      }
     };
 
     runOneSignal();
   }, []);
 
-  // This component doesn't render any visible UI of its own
   return null; 
 }
